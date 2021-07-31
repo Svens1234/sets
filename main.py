42502def0de8995f1c8b4d86ee1d4c5c50b4ccd6
Svenden123 <buhrus1@yandex.ru>
2021-07-31 21:34:11 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+rainbow_colors = {'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'}
+print(rainbow_colors)
+print(type(rainbow_colors))
+
+empty_set = set()
+print(empty_set)
+print(type(empty_set))
+
+number_list = [1, 43, 56]
+text_tuple = ('sfds', 'aafg', 'afadsf')
+set_from_list = set(number_list)
+set_from_tuple = set(text_tuple)
+
+set_from_list.add(777)
+set_from_tuple.add('hello')
+
+#set_from_list.pop() #удаляет случайный элемент
+#set_from_list.remove(43) #удаляет конкретный элемент
+#set_from_list.discard(1) #удаляет конкретный элемент
+set_from_list.clear()
+
+
+print(set_from_list)
+print(set_from_tuple)
